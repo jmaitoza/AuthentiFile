e9bba123213491cb47bde9c5ef05650e3e66a134
Joshua Maitoza <60358495+jmaitoza@users.noreply.github.com>
2023-11-21 12:23:08 -0500

    Refactored how userInput is exception handled. The program should now not crash upon entry of bad data and instead prompts the user to try again. Need to check how much this impacts performance.

--- AuthentiFile/AuthentiFile.cs
+++ AuthentiFile/AuthentiFile.cs
@@ -1,5 +1,5 @@
 ﻿using Newtonsoft.Json;
-using System.Drawing;
+using NativeFileDialogSharp;
 
 namespace AuthentiFile;
 
@@ -11,43 +11,80 @@ public class AuthentiFile
     
     static void ListMasqueradedFiles(string dirPath)
     {
-        // get all the files in the selected directory
-        foreach (var filePath in Directory.GetFiles(dirPath))
+        if (Directory.Exists(dirPath)) // ensure directory exists before listing files
         {
-            // get file extension listed in file name
-            var fileExtInName = Path.GetExtension(filePath).ToLower();
-            // fetches the first matching file signature based on the file extension in the name
-            var fileSignature = fileSigs.FirstOrDefault(fileSigs => fileSigs.Ext == fileExtInName);
-
-            if (fileSignature != null)
+            // get all the files in the selected directory
+            foreach (var filePath in Directory.GetFiles(dirPath))
             {
-                // stream in files from given directory
-                using var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
-                var buffer = new byte[fileSignature.FileSig.Count];
-                fileStream.Read(buffer, 0, buffer.Count());
-
-                if (!buffer.SequenceEqual(fileSignature.FileSig)) //check if two buffers are equal
+                // get file extension listed in file name
+                var fileExtInName = Path.GetExtension(filePath).ToLower();
+                // fetches the first matching file signature based on the file extension in the name
+                var fileSignature = fileSigs.FirstOrDefault(fileSigs => fileSigs.Ext == fileExtInName);
+    
+                if (fileSignature != null)
                 {
-                    // if not equal, print out the file's name and its file path
-                    string fileName = Path.GetFileName(filePath);
-                    string absoluteFilePath = Path.GetFullPath(filePath);
-                    Console.WriteLine($"File Name: {fileName}, File Path: {absoluteFilePath}");
+                    // stream in files from given directory
+                    using var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+                    var buffer = new byte[fileSignature.FileSig.Count];
+                    fileStream.Read(buffer, 0, buffer.Count());
+    
+                    if (!buffer.SequenceEqual(fileSignature.FileSig)) //check if two buffers are equal
+                    {
+                        // if not equal, print out the file's name and its file path
+                        string fileName = Path.GetFileName(filePath);
+                        string absoluteFilePath = Path.GetFullPath(filePath);
+                        Console.WriteLine($"File Name: {fileName}, File Path: {absoluteFilePath}");
+                    }
                 }
-            }
+            }    
         }
+
+        else
+        {
+            Console.WriteLine($"Cannot list files in {dirPath} because it does not exist.");
+        }
+        
     }
 
-    static void GetUserDir()
+    private static string GetUserDirectory()
     {
-        var fileContent = string.Empty;
-        var filePath = string.Empty;
+        while (true)
+        {
+            Console.Write("Please enter the full path of the directory you would like to search or type 'quit' to quit the program: ");
+            string userPath = Console.ReadLine();
+
+            if (userPath?.ToLower() == "quit" || userPath?.ToLower() == "q")
+            {
+                Console.WriteLine("Closing program...");
+                Environment.Exit(0);
+            }
+            
+            // check if input is null
+            else if (string.IsNullOrEmpty(userPath))
+            {
+                Console.WriteLine("Please enter a directory.");
+            }
+            
+            // check if user input a file instead of a directory
+            else if (File.Exists(userPath))
+            {
+                Console.WriteLine("Please enter a directory, not a file.");
+            }
+            
+            else
+            {
+                // Treat as relative path and return the absolute path
+                return Path.GetFullPath(userPath);
+            }
+        }
+        
     }
 
     static void Main(string[] args)
     {
-        
-        ListMasqueradedFiles("/Users/joshuamaitoza/RiderProjects/AuthentiFile/testFiles"); //macbook path
-        // ListMasqueradedFiles("C:\\Users\\joshm\\RiderProjects\\AuthentiFile\\testFiles"); // windows pc path
+        while (true)
+        {
+            ListMasqueradedFiles(GetUserDirectory());
+        }
     }
-
 }
